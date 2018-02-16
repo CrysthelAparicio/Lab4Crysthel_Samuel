7a19c9419feabb1c17247b94e6e3fa1bc4b08f19
CrysthelAparicio <crysthel.aparicio@unitec.edu>
2018-02-16 14:32:20 -0600

    reviseo

--- Lab4Samuel_Crysthel/src/lab4samuel_crysthel/Lab4Samuel_Crysthel.java
+++ Lab4Samuel_Crysthel/src/lab4samuel_crysthel/Lab4Samuel_Crysthel.java
@@ -18,46 +18,5 @@ public class Lab4Samuel_Crysthel {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
-    public Lab4Samuel_Crysthel(){
-        String opcion;
-        do {
-            opcion = JOptionPane.showInputDialog("MENU:\n"
-                    + "2) Agregar\n"
-                    + "2) Modificar\n"
-                    + "3) Eliminar\n"
-                    + "4) Permiso lanzar viaje\n"
-                    + "s) Salir");
-            switch (opcion) {
-                case "1":
-                {
-                    String tipoGuerrero = JOptionPane.showInputDialog("Que tipo de guerrero desea crear?\n"
-                            + "(mago, elfo, dragon, arquero, bruja): ");
-                    switch (tipoGuerrero) {
-                        case "mago":
-                            baseGuerreros.add(new Mago());
-                            break;
-                        case "elfo":
-                            baseGuerreros.add(new Elfo());
-                            break;
-                        case "dragon":
-                            baseGuerreros.add(new Dragon());
-                            break;
-                        case "arquero":
-                            baseGuerreros.add(new Arquero());
-                            break;
-                        case "bruja":
-                            baseGuerreros.add(new Bruja());
-                            break;
-                        case "s":
-                            break;
-                        default:
-                            JOptionPane.showMessageDialog(null, "Opcion no valida! cancelando...");
-                            break;
-                    }
-                }
-                    break;
-    }
-    
+    }   
 }
