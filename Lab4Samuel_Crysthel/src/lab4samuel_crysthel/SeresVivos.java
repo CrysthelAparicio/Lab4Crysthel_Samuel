faa68cd4b47052b946c18b82be6a95c280d829a1
CrysthelAparicio <crysthel.aparicio@unitec.edu>
2018-02-16 13:34:26 -0600

    Empezando

---
+++ Lab4Samuel_Crysthel/src/lab4samuel_crysthel/SeresVivos.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4samuel_crysthel;
+
+/**
+ *
+ * @author COPECO -13
+ */
+public class SeresVivos {
+    protected String nombre;
+    protected String grupo;
+}
